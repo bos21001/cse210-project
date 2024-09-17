444cdc22f05e15538651a9f03a342d3255ea7e48
--global <--global>
2024-09-17 21:54:39 +0200

    Finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+        
+        while (playAgain)
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int guess = -1;
+            int guesses = 0;
+
+            while (magicNumber != guess)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+            
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            
+                guesses++;
+            }
+        
+            Console.WriteLine($"You guessed the magic number {magicNumber} in {guesses} guesses!");
+            
+            Console.Write("Do you want to play again? (y/n) ");
+            string playAgainInput = Console.ReadLine(); 
+            
+            playAgain = playAgainInput == "y";
+        }
+        
+        Console.WriteLine("Thanks for playing!");
     }
 }
