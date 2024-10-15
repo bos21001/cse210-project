695f596ef4ffb3de3471a6ec4f23b951b4fe30ef
--global <--global>
2024-10-15 13:03:46 +0200

    W05 Prepare: Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
-using System;
+namespace Learning05;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment = new Assignment("Christopher Mendes", "Math");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+        
+        MathAssignment mathAssignment = new MathAssignment("Someone Test", "Math", "1.1", "1-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+        
+        WritingAssignment writingAssignment = new WritingAssignment("Rafaela Mendes", "Writing", "The Best Writing Assignment");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
