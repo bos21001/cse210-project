f5b9836a2fb3702aa15251385d32fd69718386dc
--global <--global>
2024-10-30 15:07:42 +0100

    Polymorphism Learning Activity

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,18 @@
-using System;
+namespace learning06;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 5, 10));
+        shapes.Add(new Circle("Green", 5));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Area of {shape.GetColor()} shape is {shape.GetArea()}");
+        }
     }
 }
